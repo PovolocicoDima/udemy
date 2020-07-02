bef994ada0b643b1f85439164c2ed6750127d89e
Dima <povolocico.dima@gmail.com>
2020-07-02 13:27:53 +0300

    finish 30 lesson, end of practice: mars

--- dom/js/script.js
+++ dom/js/script.js
@@ -1,23 +1,57 @@
 "use strict";
 
- const box = document.getElementById('box');
- console.log(box);
+ const box = document.getElementById('box'),
+       btns = document.getElementsByTagName('button'),
+       circles = document.getElementsByClassName('circle'),
+       wrapper = document.querySelector('.wrapper'),
+       hearts = document.querySelectorAll('.heart'),
+       oneHeart = document.querySelector('.heart');
+       
 
- const btns = document.getElementsByTagName('button');
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
 
- console.log(btns[1]);
-
- const circles = document.getElementsByClassName('circle');
- console.log(circles);
+box.style.cssText = `background-color: blue; width: 500px`;
 
 
- const hearts = document.querySelectorAll('.heart');
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
- console.log(hearts);
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
 
- hearts.forEach(item => {
-     console.log(item);
- });
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
 
- const oneHeart = document.querySelector('.heart');
- console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+wrapper.append(div);
+
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+
+// hearts[0].before(div);
+
+// hearts[0].after(div);
+
+// wrapper.insertBefore(div, hearts[1]);
+
+// circles[0].remove();
+
+// wrapper.removeChild(hearts[1]);
+
+// hearts[0].replaceWith(circles[0]);
+
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = "<h1>Hellow WOrld!</h1>";
+
+// div.textContent = "<h1>Hellow WOrld!</h1>";
+
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
