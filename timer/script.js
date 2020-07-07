ccb744eea3f8eb4624a90b87d8d9b927b6f8d327
Dima <povolocico.dima@gmail.com>
2020-07-07 14:29:47 +0300

    простая анимация

--- timer/script.js
+++ timer/script.js
@@ -1,11 +1,37 @@
 const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
 
-btn.addEventListener('click', () => {
-    const timerId = setTimeout(logger, 2000);
-});
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
 
-// clearInterval(timerId);
+    const id = setInterval(frame, 10);
 
-function logger() {
-    console.logger('text');
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
 }
+
+
+
+btn.addEventListener('click', myAnimation);
+
+// function logger() {
+//     if (i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++;
+// }
+
+// let id = setTimeout(function log() {
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+//  }, 500);
