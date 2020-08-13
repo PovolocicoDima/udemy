5039ad8f7435bc6d4fe98c43c7a2e9cfea12f23e
Dima <povolocico.dima@gmail.com>
2020-08-13 21:49:19 +0300

    добавлена адаптивность для сайта

--- verstka/gulpfile.js
+++ verstka/gulpfile.js
@@ -1,46 +1,33 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass        = require('gulp-sass');
-const rename      = require("gulp-rename");
-const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
 
-// Static server
 gulp.task('server', function() {
-    browserSync.init({
+
+    browserSync({
         server: {
-            baseDir: "/home/povolochko/udemy_course/verstka/bootstrap_project"
+            baseDir: "bootstrap_project"
         }
     });
-});
 
-// or...
-
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        proxy: "yourlocal.dev"
-    });
+    gulp.watch("./bootstrap_project/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("bootstrap_projec*.+(scss|sass)")
+    return gulp.src("./bootstrap_project/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min"
-          }))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("bootstrap_projec/css"))
+        .pipe(gulp.dest("./bootstrap_project/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("bootstrap_projec*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch(".*.html").on("change", browserSync.reload);
-});
+    gulp.watch("./bootstrap_project/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+})
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
