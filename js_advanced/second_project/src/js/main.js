875c4058f1b77d67c9946b06e63dfa86b7fe605f
Dima <povolocico.dima@gmail.com>
2020-09-02 18:07:08 +0300

    lesson 21, added accordion for question forms, did 2 versions with css and js in second project

--- js_advanced/second_project/src/js/main.js
+++ js_advanced/second_project/src/js/main.js
@@ -6,8 +6,12 @@ import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
-// import filterTest from './modules/filter_test';
 import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
+
+
+
+// import filterTest from './modules/filter_test';
 // import pictureSizedTest from './modules/pictureSizedTest';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -23,7 +27,15 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
-    // filterTest();
     pictureSize('.sizes-block');
+    accordion('.accordion-heading');
+    
+    
+    
+    
+    
+    
+    
+    // filterTest();
     // pictureSizedTest('.sizes-block');
 });
