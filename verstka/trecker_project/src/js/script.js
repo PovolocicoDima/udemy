aa705afbf4681abe387972c7ec3675eacf9df450
Dima <povolocico.dima@gmail.com>
2020-09-19 18:41:42 +0300

    added slider for trecker project

---
+++ verstka/trecker_project/src/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg"></button>',
+        dots: false,
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+  });
