9724a8073b87f107562e930720578fd97100a614
Dima <povolocico.dima@gmail.com>
2020-09-20 17:26:52 +0300

    added tabs content to trecker project

--- verstka/trecker_project/src/js/script.js
+++ verstka/trecker_project/src/js/script.js
@@ -1,41 +1,62 @@
 $(document).ready(function(){
+    // slider
     $('.carousel__inner').slick({
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg"></button>',
-        dots: false,
-        arrows: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            }
+      infinite: true,
+      speed: 1000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg"></button>',
+      dots: false,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
           }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-    });
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
   });
+
+  // tabs
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+  
+  // tabs content
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  }
+
+  toggleSlide('.catalog-item__list-link');
+  toggleSlide('.catalog-item__link');
+});
