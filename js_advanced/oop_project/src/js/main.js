136b1d87d474c2658dece30f3c33f796f328fecb
Dima <povolocico.dima@gmail.com>
2020-10-15 19:12:19 +0300

    added form logic for oop_project

--- js_advanced/oop_project/src/js/main.js
+++ js_advanced/oop_project/src/js/main.js
@@ -2,6 +2,7 @@ import MainSlider from './modules/slider/slider-main';
 import MiniSlider from './modules/slider/slider-mini';
 import Player from './modules/playVideo';
 import Difference from './modules/difference';
+import Forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({container: '.page', btns: '.next'});
@@ -39,4 +40,5 @@ window.addEventListener('DOMContentLoaded', () => {
     player.init();
     
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
+    new Forms('.form').init();
 });
