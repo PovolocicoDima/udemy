05758a7655bc91cc98d5ccacc3470ffea457335c
Dima <povolocico.dima@gmail.com>
2020-11-18 16:10:55 +0200

    react forms

--- course_react/react-apps/todo/src/components/todo-list-item/todo-list-item.js
+++ course_react/react-apps/todo/src/components/todo-list-item/todo-list-item.js
@@ -6,15 +6,10 @@ export default class TodoListItem extends Component {
 
   render() {
 
-<<<<<<< HEAD
     const { label, onDeleted,
             onToggleImportant,
             onToggleDone,
             important, done } = this.props;
-=======
-    const { label, onDeleted, onToggleDone, onToggleImportant,
-    important, done } = this.props;
->>>>>>> 6a7c04b9f3a8b2876a78ca022e61bfdda167f133
 
     let classNames = 'todo-list-item';
     if (done) {
@@ -29,27 +24,19 @@ export default class TodoListItem extends Component {
       <span className={ classNames }>
         <span
           className="todo-list-item-label"
-<<<<<<< HEAD
-          onClick={onToggleDone}>
-=======
           onClick={ onToggleDone }>
->>>>>>> 6a7c04b9f3a8b2876a78ca022e61bfdda167f133
           {label}
         </span>
 
         <button type="button"
                 className="btn btn-outline-success btn-sm float-right"
-<<<<<<< HEAD
-                onClick={onToggleImportant}>
-=======
                 onClick={ onToggleImportant }>
->>>>>>> 6a7c04b9f3a8b2876a78ca022e61bfdda167f133
           <i className="fa fa-exclamation" />
         </button>
 
         <button type="button"
                 className="btn btn-outline-danger btn-sm float-right"
-                onClick={onDeleted}>
+                onClick={ onDeleted }>
           <i className="fa fa-trash-o" />
         </button>
       </span>
--- course_react/react-apps/todo/src/components/todo-list/todo-list.js
+++ course_react/react-apps/todo/src/components/todo-list/todo-list.js
@@ -3,13 +3,7 @@ import React from 'react';
 import TodoListItem from '../todo-list-item';
 import './todo-list.css';
 
-<<<<<<< HEAD
-const TodoList = ({ todos, onDeleted,
-                  onToggleImportant,
-                  onToggleDone }) => {
-=======
 const TodoList = ({ todos, onDeleted, onToggleDone, onToggleImportant }) => {
->>>>>>> 6a7c04b9f3a8b2876a78ca022e61bfdda167f133
 
   const elements = todos.map((item) => {
     const { id, ...itemProps } = item;
@@ -18,15 +12,9 @@ const TodoList = ({ todos, onDeleted, onToggleDone, onToggleImportant }) => {
       <li key={id} className="list-group-item">
         <TodoListItem
           {...itemProps }
-<<<<<<< HEAD
           onDeleted={() => onDeleted(id)}
           onToggleImportant={() => onToggleImportant(id)}
           onToggleDone={() => onToggleDone(id)}
-=======
-          onDeleted={() => onDeleted(id)} 
-          onToggleDone={() => onToggleDone(id)}  
-          onToggleImportant={() => onToggleImportant(id)}  
->>>>>>> 6a7c04b9f3a8b2876a78ca022e61bfdda167f133
         />
       </li>
     );
