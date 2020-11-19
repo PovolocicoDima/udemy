7b3746555832d466faf96d4c18e5b827f3bb46e9
Dima <povolocico.dima@gmail.com>
2020-11-19 08:54:00 +0200

    final version off todo-app

--- course_react/react-apps/todo/src/components/app/app.js
+++ course_react/react-apps/todo/src/components/app/app.js
@@ -18,7 +18,8 @@ export default class App extends Component {
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch')
     ], 
-    term: ''
+    term: '',
+    filter: 'all'  // all, active, done
   };
 
   createTodoItem(label) {
@@ -108,34 +109,56 @@ export default class App extends Component {
   };
 
   onSearchChange = (term) => {
-    this.setState(({term}));
+    this.setState(({ term }));
   };
+
+  onFilterChange = (filter) => {
+    this.setState(({ filter }));
+  };
+
+  filter(items, filter) {
+    
+    switch(filter) {
+      case 'all':
+        return items;
+      case 'active':
+        return items.filter((item) => !item.done);
+      case 'done':
+        return items.filter((item) => item.done);
+      default:
+        return items;
+    }
+  }
 
   render() {
 
-    const { todoData, term } = this.state;
+    const { todoData, term, filter } = this.state;
     // filter items for search panel
-    const visibleItems = this.search(todoData, term);
+    const visibleItems = this.filter(
+      this.search(todoData, term), filter);
+
     const doneCount = todoData
                       .filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
 
     return (
       <div className="todo-app">
-        <AppHeader toDo={todoCount} done={doneCount} />
+        <AppHeader toDo={ todoCount } done={ doneCount } />
         <div className="top-panel d-flex">
           <SearchPanel 
-            onSearchChange={this.onSearchChange}/>
-          <ItemStatusFilter />
+            onSearchChange={ this.onSearchChange }/>
+          <ItemStatusFilter 
+            filter={ filter }
+            onFilterChange={ this.onFilterChange }/>
         </div>
 
         <TodoList
-          todos={visibleItems}
+          todos={ visibleItems }
           onDeleted={ this.deleteItem }
-          onToggleImportant={this.onToggleImportant}
-          onToggleDone={this.onToggleDone}
+          onToggleImportant={ this.onToggleImportant }
+          onToggleDone={ this.onToggleDone }
         />
-        <ItemAddForm onItemAdded={this.addItem}/>
+        <ItemAddForm onItemAdded={ this.addItem }/>
       </div>
     );
   }
