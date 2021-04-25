e7efe35825a5630a9ca6f84a4c386c7956203d92
Povolocico Dmitrii <povolocico.dima@gmail.com>
2021-04-25 16:38:51 +0300

    new express project

---
+++ node_JS/node_js_for_beginners/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import fileUpload from 'express-fileupload';
+import router from './router.js';
+
+
+const PORT = 3000;
+const DB_URL = 'mongodb+srv://dima:dima@cluster0.pgft0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const app = express();
+
+app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+
+async function startApp() {
+	try {
+		await mongoose.connect(DB_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true 
+		})
+		app.listen(PORT, () => console.log(`SERVER STARTED ON ${PORT} PORT`))
+	} catch(e) {
+		console.log(e);
+	}
+}
+
+startApp();
