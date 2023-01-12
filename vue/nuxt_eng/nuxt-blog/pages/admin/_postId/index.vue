37a7c41a96e569293b85f372bceb0449a1bb34d0
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2023-01-12 17:45:01 +0200

    Jan12 work

---
+++ vue/nuxt_eng/nuxt-blog/pages/admin/_postId/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm.vue';
+
+export default {
+  components: {
+    AdminPostForm,
+  },
+  layout: 'admin',
+  data() {
+    return {
+      loadedPost: {
+        author: 'Dima',
+        title: 'My post1',
+        content: 'super',
+        thumbnailLink: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
